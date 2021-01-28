343dfd6b14174f0bff4ce0757e256229bb4995fa
Mark Force <force.j.mark@gmail.com>
2021-01-28 13:25:41 -0500

    added content to contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,6 +6,9 @@ const Contact = () => {
         <div className='contact'>
             <div className='page-wrapper'>
                 <h2 className='page-header'>Contact Us</h2>
+                <p>Contact Us by Email</p>
+                <span className='contact-info'>ninjacam@aol.com</span>
+                <span className='contact-info'>mark.lightningjib@gmail.com</span>
             </div>
         </div>
     )
