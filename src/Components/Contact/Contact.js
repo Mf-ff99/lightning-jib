062b128145b6ac2e1c49d6c832597c50fd17fb49
Mark Force <force.j.mark@gmail.com>
2021-01-30 11:49:55 -0500

    swapped email locations on contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -7,8 +7,8 @@ const Contact = () => {
             <div className='page-wrapper'>
                 <h2 className='page-header'>Contact Us</h2>
                 <p>Contact Us by Email</p>
-                <span className='contact-info'>ninjacam@aol.com</span>
                 <span className='contact-info'>mark.lightningjib@gmail.com</span>
+                <span className='contact-info'>ninjacam@aol.com</span>
             </div>
         </div>
     )
